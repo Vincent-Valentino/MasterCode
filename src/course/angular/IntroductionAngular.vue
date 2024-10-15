1b7f2456eed1f29ea14339ffc0ea6dfa799c0625
Vincent-Valentino <valentinolubu2@gmail.com>
2024-10-15 16:49:30 +0700

    correct path img

--- src/course/angular/IntroductionAngular.vue
+++ src/course/angular/IntroductionAngular.vue
@@ -2,7 +2,7 @@
   <div class="w-full">
     <!-- Header Section -->
     <div class="w-full flex items-center gap-2 bg-red-600 rounded-t-xl p-4">
-      <img src="/angular/icons.svg" alt="Angular Logo" class="w-12 h-12">
+      <img src="/angular/logo.svg" alt="Angular Logo" class="w-12 h-12">
       <h1 class="text-2xl md:text-4xl font-bold text-white">Introduction to Angular</h1>
     </div>
 
